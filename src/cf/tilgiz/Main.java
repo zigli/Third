0bc4db301e115dbb626a14322e3f6e62f027c7d9
ilgiz <4ilgiz@gmail.com>
2019-01-21 16:24:33 +0300

    Constuctors

--- src/cf/tilgiz/Main.java
+++ src/cf/tilgiz/Main.java
@@ -4,6 +4,13 @@ public class Main {
 
     public static void main(String[] args) {
 	    say();
+	    Person vitya = new Person(180);
+	    vitya.say2("ilgiz");
+        System.out.println(vitya.height);
+        Person zigli = new Person(120);
+        System.out.println(zigli.height);
+        Person slava = new Person();
+        slava.height = 150;
     }
 
     static void  say(){
--- src/cf/tilgiz/Person.java
+++ src/cf/tilgiz/Person.java
@@ -1,5 +1,15 @@
 package cf.tilgiz;
 
 public class Person {
-    private int b = 5;
+    public int height;
+
+    Person(){}
+
+    Person(int height){
+        this.height = height;
+    }
+
+    void say2(String name){
+        System.out.println("Hello, " + name);
+    }
 }
